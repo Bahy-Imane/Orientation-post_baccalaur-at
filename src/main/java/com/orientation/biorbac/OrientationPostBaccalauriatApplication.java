855b8b03118df63d3ec7e683b90a1b45368e26e3
I M A N E <imanebahy91@gmail.com>
2024-08-08 09:47:27 +0100

    project architecture

---
+++ src/main/java/com/orientation/biorbac/OrientationPostBaccalauriatApplication.java
@@ -0,0 +1,13 @@
+package com.orientation.biorbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientationPostBaccalauriatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientationPostBaccalauriatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orientation/biorbac/OrientationPostBaccalauriatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orientation.biorbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientationPostBaccalauriatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
