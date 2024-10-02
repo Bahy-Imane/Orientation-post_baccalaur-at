ceb858ae707853002e1983ffc62fdb9bbd405fc6
I M A N E <imanebahy91@gmail.com>
2024-10-02 09:23:42 +0100

    sign up design

--- orientation-post-baccalauriat-FrontEnd/src/app/app.routes.ts
+++ orientation-post-baccalauriat-FrontEnd/src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {HomeComponent} from "./components/home/home.component";
-import {SignUpComponent} from "./components/sign-up/sign-up.component";
 import {AboutUsComponent} from "./components/about-us/about-us.component";
 import {ContactUsComponent} from "./components/contact-us/contact-us.component";
+import {SignUpComponent} from "./components/sign-up/sign-up.component";
 
 export const routes: Routes = [
   {path : '' ,component : LoginComponent},
+  {path : 'login' ,component : LoginComponent},
   {path : 'home' , component : HomeComponent},
-  {path : 'signUp',component : SignUpComponent},
+  { path: 'signUp', component: SignUpComponent },
   {path : 'about' , component :AboutUsComponent},
   {path : 'contact' , component :ContactUsComponent},
 
--- orientation-post-baccalauriat-FrontEnd/src/app/components/sign-up/sign-up.component.ts
+++ orientation-post-baccalauriat-FrontEnd/src/app/components/sign-up/sign-up.component.ts
@@ -1,19 +1,39 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router, RouterLink} from '@angular/router';
 import { BacType } from "../../core/enums/bac-type";
 import { AuthService } from "../../core/services/auth.service";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {MatFormField} from "@angular/material/form-field";
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css'],
+  imports: [
+    ReactiveFormsModule,
+    NgClass,
+    NgIf,
+    MatFormField,
+    MatSelect,
+    MatOption,
+    NgForOf,
+    MatProgressSpinner,
+    RouterLink,
+    MatInput,
+    MatButton
+  ]
 })
 export class SignUpComponent {
-  signupForm: FormGroup;
+  signUpForm!: FormGroup;
   bacTypes = Object.values(BacType);
   loading = false;
+  success = false;
   failure = false;
   errorMessage: string | null = null;
 
@@ -21,7 +41,7 @@ export class SignUpComponent {
   private router = inject(Router);
 
   constructor(private fb: FormBuilder) {
-    this.signupForm = this.fb.group({
+    this.signUpForm = this.fb.group({
       userName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -33,49 +53,50 @@ export class SignUpComponent {
   }
 
   onSignup() {
-    if (this.signupForm.valid) {
+    if (this.signUpForm.valid) {
       this.loading = true;
-      const userDto = this.signupForm.value;
+      const userDto = this.signUpForm.value;
 
       this.authService.signUp(userDto).subscribe({
         next: (response) => {
           this.loading = false;
+          this.success = true;
           this.router.navigate(['/login']);
         },
         error: (error) => {
           this.loading = false;
           this.failure = true;
-          this.errorMessage = 'Échec de l\'inscription. Veuillez réessayer.'; // Vous pouvez personnaliser ce message
+          this.errorMessage = 'Signup failed. Please try again.';
         }
       });
     }
   }
 
   get userName() {
-    return this.signupForm.get('userName');
+    return this.signUpForm.get('userName');
   }
 
   get email() {
-    return this.signupForm.get('email');
+    return this.signUpForm.get('email');
   }
 
   get password() {
-    return this.signupForm.get('password');
+    return this.signUpForm.get('password');
   }
 
   get dateOfBirth() {
-    return this.signupForm.get('dateOfBirth');
+    return this.signUpForm.get('dateOfBirth');
   }
 
   get bacScore() {
-    return this.signupForm.get('bacScore');
+    return this.signUpForm.get('bacScore');
   }
 
   get location() {
-    return this.signupForm.get('location');
+    return this.signUpForm.get('location');
   }
 
   get bacType() {
-    return this.signupForm.get('bacType');
+    return this.signUpForm.get('bacType');
   }
 }
